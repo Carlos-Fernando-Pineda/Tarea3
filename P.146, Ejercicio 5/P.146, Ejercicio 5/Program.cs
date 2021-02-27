b0aa2541d818a51b7f42c74fa7394ccf806d35a6
Carlos-Fernando-Pineda <79342082+Carlos-Fernando-Pineda@users.noreply.github.com>
2021-02-27 11:39:02 -0600

    Add files via upload

---
+++ P.146, Ejercicio 5/P.146, Ejercicio 5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace P._146__Ejercicio_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int per1, per2, per3;
+            Console.Write("Introduzca la edad de la primera persona: ");
+            per1 = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca la edad de la segunda persona: ");
+            per2 = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca la edad de la tercera persona: ");
+            per3 = int.Parse(Console.ReadLine());
+
+            if (per1 > per2 && per1 > per3)
+            {
+                Console.WriteLine("La mayor edad es " + per1);
+            }
+            else if (per2 > per1 && per2 > per3)
+            {
+                Console.WriteLine("La mayor edad es " + per2);
+            }
+            else if (per3>per1 && per3 > per2)
+            {
+                Console.WriteLine("La mayor edad es " + per3);
+            }
+            if (per1<per2 && per1 < per3)
+            {
+                Console.WriteLine("La menor edad es " + per1);
+            }
+            else if(per2<per1 && per2 < per3)
+            {
+                Console.WriteLine("La menor edad es " + per2);
+            }
+            else if(per3<per1 && per3 < per1)
+            {
+                Console.WriteLine("La menor edad es " + per3);
+            }
+        }
+    }
+}
